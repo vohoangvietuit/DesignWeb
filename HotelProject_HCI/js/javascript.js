7b299c6ac171adb8a96095a728d15664a787ec20
vohoangvietuit <vohoangvietuit@gmail.com>
2016-12-01 23:09:34 +0700

    1/12/2016 update Hotel & nhung cai nho

--- HotelProject_HCI/js/javascript.js
+++ HotelProject_HCI/js/javascript.js
@@ -26,3 +26,49 @@ $(document).ready(function() {
         }
     });
 });
+
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 1200) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});
+
+        jQuery(document).ready(function($) {
+            var $filter = $('.head_nav');
+            var $filterSpacer = $('<div />', {
+                "class": "vnkings-spacer",
+                "height": $filter.outerHeight()
+            });
+            if ($filter.size())
+            {
+                $(window).scroll(function ()
+                {
+                    if (!$filter.hasClass('fix') && $(window).scrollTop() > $filter.offset().top)
+                    {
+                        $filter.before($filterSpacer);
+                        $filter.addClass("fix");
+                    }
+                    else if ($filter.hasClass('fix')  && $(window).scrollTop() < $filterSpacer.offset().top || $(window).scrollTop() < 100)
+                    {
+                        $filter.removeClass("fix");
+                        $filterSpacer.remove();
+                    }
+                });
+            }
+ 
+        });
