aac4a9a0fee0c591df571d49c788f6d5cb6a7bfe
vohoangvietuit <vohoangvietuit@gmail.com>
2017-03-07 11:51:28 +0700

    Cap nhat them Myproject 7/3

---
+++ A_MyProject - Tự thiết kế/js/myjs.js
@@ -0,0 +1,50 @@
+﻿ $(function(){
+	 $(window).scroll(function(){
+	 	vitrihientai = $('body').scrollTop();
+	 	if(vitrihientai > 300){
+	 		$('.navbar').addClass('tienhoa')
+	 	}
+	 	else if(vitrihientai < 300){
+	 		$('.navbar').removeClass('tienhoa')
+	 	}
+	 })
+
+	 var docao = $(window).height()
+	 $(".gioithieu").css({'height': docao})
+
+	 $(window).resize(function(){
+		 var docao = $(window).height()
+		 $(".gioithieu").css({'height': docao})
+	 })
+
+	 $('.n1').click(function(){
+	 	$('body').animate({scrollTop: 0}, 2000, "easeInOutExpo")
+	 })
+	 $('.n2').click(function(){
+	 	$('body').animate({scrollTop: $('#gioithieu1').offset().top - 100}, 2000, "easeInOutExpo")
+	 })
+	 $('.n3').click(function(){
+	 	$('body').animate({scrollTop: $('#giaohang').offset().top}, 2000, "easeInOutExpo")
+	 })
+	 $('.n4').click(function(){
+	 	$('body').animate({scrollTop: $('#lienhe').offset().top}, 2000, "easeInOutExpo")
+	 })
+	 $('.chohang').click(function(){
+	 	$('body').animate({scrollTop: $('#giaohang').offset().top}, 2000, "easeInOutExpo")
+	 	return false;
+	 })
+
+	 $('.truckrun').click(function(){
+	 	// $('.truckrun').addClass("wow bounceOutLeft");
+	 	$('.truckrun').attr("style","visibility: visible; animation-name: bounceOutLeft; animation-duration: 2s");
+	 })
+	 $(window).scroll(function(){
+	 	if($('body').scrollTop() > $('.truckrun').offset().top)
+	 	{
+	 		// $('.truckrun').removeClass('wow bounceOutLeft');
+	 		$('.truckrun').removeAttr("style","visibility: visible; animation-name: bounceOutLeft;");
+	 	}
+	 })
+	 new WOW().init();
+
+})
---
+++ Chuyển PSD sang HTML-CSS/1/1.js
@@ -0,0 +1,41 @@
+﻿ $(function(){
+ 	//ham click
+ 	$('.n1').click(function(){
+ 		$('body').animate({scrollTop:$('#sanpham').offset().top}, 2000, "easeInOutExpo");
+ 		return false;
+ 	})
+ 	$('.n2').click(function(){
+ 		$('body').animate({scrollTop:$('#thongtin').offset().top});
+ 		return false;
+ 	})
+ 	$('.n3').click(function(){
+ 		$('body').animate({scrollTop:$('#lienhe').offset().top});
+ 		return false;
+ 	})
+
+ 	//cuon chuoi thu nho menu
+	 $(window).scroll(function(){
+	 	vitrihientai = $('body').scrollTop();
+	 	console.log(vitrihientai);
+
+	 	if(vitrihientai > 330)
+	 	{
+	 		$('.navbar').addClass('tienhoa');
+	 	}
+	 	else if(vitrihientai < 330)
+	 	{
+	 		$('.navbar').removeClass('tienhoa');
+	 	}
+	 	//hieu thi sp
+	 	if(vitrihientai > 490)
+	 	{
+	 		$('.portfolio').addClass('bienra');
+	 	}	 	
+	 	else if(vitrihientai < 490)
+	 	{
+	 		$('.portfolio').removeClass('bienra');
+	 	}
+
+	 })
+})  
+ 
