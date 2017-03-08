9e34fe924673c1ca7754f34a36d3d03487d232ff
vohoangvietuit <vohoangvietuit@gmail.com>
2017-03-08 09:51:14 +0700

    Cap nhat MyProject 8/3

--- A_MyProject - Tự thiết kế/js/myjs.js
+++ A_MyProject - Tự thiết kế/js/myjs.js
@@ -24,19 +24,22 @@
 	 	$('body').animate({scrollTop: $('#gioithieu1').offset().top - 100}, 2000, "easeInOutExpo")
 	 })
 	 $('.n3').click(function(){
-	 	$('body').animate({scrollTop: $('#giaohang').offset().top}, 2000, "easeInOutExpo")
+	 	$('body').animate({scrollTop: $('#giaohang').offset().top + 30}, 2000, "easeInOutExpo")
 	 })
 	 $('.n4').click(function(){
 	 	$('body').animate({scrollTop: $('#lienhe').offset().top}, 2000, "easeInOutExpo")
 	 })
+	  $('.n5').click(function(){
+	 	$('body').animate({scrollTop: $('#tintuc').offset().top}, 2000, "easeInOutExpo")
+	 })
 	 $('.chohang').click(function(){
-	 	$('body').animate({scrollTop: $('#giaohang').offset().top}, 2000, "easeInOutExpo")
+	 	$('body').animate({scrollTop: $('#giaohang').offset().top +30}, 2000, "easeInOutExpo")
 	 	return false;
 	 })
 
 	 $('.truckrun').click(function(){
 	 	// $('.truckrun').addClass("wow bounceOutLeft");
-	 	$('.truckrun').attr("style","visibility: visible; animation-name: bounceOutLeft; animation-duration: 2s");
+	 	$('.truckrun').attr("style","visibility: visible; animation-name: bounceOutLeft; animation-duration: 2.5s");
 	 })
 	 $(window).scroll(function(){
 	 	if($('body').scrollTop() > $('.truckrun').offset().top)
@@ -45,6 +48,36 @@
 	 		$('.truckrun').removeAttr("style","visibility: visible; animation-name: bounceOutLeft;");
 	 	}
 	 })
+
+	 // $('.chohang').click(function(){
+	 // 	// $('.truckrun').addClass("wow bounceOutLeft");
+	 // 	$('.chohang').attr("style","visibility: visible; animation-name: bounceOutRight; animation-duration: 4s");
+	 // })
+	 // $(window).scroll(function(){
+	 // 	if($('body').scrollTop() > $('.chohang').offset().top)
+	 // 	{
+	 // 		// $('.truckrun').removeClass('wow bounceOutLeft');
+	 // 		$('.chohang').removeAttr("style","visibility: visible; animation-name: bounceOutRight;");
+	 // 	}
+	 // })
+
+
+	 //back to top
+	 $(window).scroll(function(){
+	 	if($(this).scrollTop() > 1000 ){
+	 		$('#back-to-top').fadeIn();
+	 	}
+	 	else{
+	 		$('#back-to-top').fadeOut();
+	 	}
+	 });
+
+	 //click to top
+	 $('#back-to-top').click(function(){
+	 	$('body').animate({scrollTop: 0});
+	 });
+
+
 	 new WOW().init();
 
 })
